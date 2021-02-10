1961cfa0236c4caa117f859ac35918258840588d
Ola Bini <ola@olabini.se>
2021-02-10 20:19:25 +0000

    Finalize notifications for issues

--- eventHandler_issues.js
+++ eventHandler_issues.js
@@ -1,19 +1,11 @@
 const core = require('@actions/core');
 
-function format_user(user) {
-    return user;
-}
-
-function format_repository(repo) {
-    return repo;
-}
-
 function repository_name(e) {
-    return e.repository.name;
+    return e.repository.full_name;
 }
 
 function repo(e) {
-    return format_repository(repository_name(e));
+    return repository_name(e);
 }
 
 function sender_name(e) {
@@ -21,7 +13,7 @@ function sender_name(e) {
 }
 
 function sender(e) {
-    return format_user(sender_name(e));
+    return sender_name(e);
 }
 
 function number(e) {
@@ -30,14 +22,6 @@ function number(e) {
 
 function title(e) {
     return e.issue.title;
-}
-
-function isOpened(e) {
-    return e.action === 'opened';
-}
-
-function isClosed(e) {
-    return e.action === 'closed';
 }
 
 function repo_url(e) {
@@ -52,19 +36,63 @@ function issue_url(e) {
     return e.issue.html_url;
 }
 
+function assignee(e) {
+    return e.assignee.login;
+}
+
+function assignee_url(e) {
+    return e.assignee.html_url;
+}
+
 function handler(e) {
-    if (isOpened(e) || isClosed(e)) {
-        const subject = `[${repo(e)}] ${sender(e)} ${e.action} issue #${number(e)}`;
-        const message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> ${e.action} issue <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i>`;
-        return {
-            subject: subject,
-            message: message,
-        };
+    let subject = '';
+    let message = '';
+    
+    switch(e.action) {
+    case 'opened':
+    case 'reopened':
+    case 'closed':
+    case 'edited':
+    case 'deleted':
+    case 'locked':
+    case 'unlocked':
+    case 'pinned':
+    case 'unpinned':
+    case 'transferred':
+        subject = `[${repo(e)}] ${sender(e)} ${e.action} issue #${number(e)}`;
+        message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> ${e.action} issue <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i>`;
+        break;
+    case 'assigned':
+    case 'unassigned':
+        let referent = 'to';
+        if (e.action === 'unassigned') {
+            referent = 'from';
+        }
+
+        subject = `[${repo(e)}] ${sender(e)} ${e.action} ${assignee(e)} ${referent} issue #${number(e)}`;
+        message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> ${e.action} <a href="${assignee_url(e)}">${assignee(e)}</a> ${referent} <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i>`;
+        break;
+    case 'labeled':
+        subject = `[${repo(e)}] ${sender(e)} added the label '${e.label.name}' to issue #${number(e)}`;
+        message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> added the label '${e.label.name}' to <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i>`;
+        break;
+    case 'unlabeled':
+        subject = `[${repo(e)}] ${sender(e)} remove the label '${e.label.name}' from issue #${number(e)}`;
+        message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> remove the label '${e.label.name}' from <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i>`;
+        break;
+    case 'milestoned':
+        subject = `[${repo(e)}] ${sender(e)} set the milestone for issue #${number(e)} to '${e.milestone.title}'`;
+        message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> set the milestone for issue <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i> to '<a href="${e.milestone.html_url}">${e.milestone.title}</a>'`;
+        break;
+    case 'demilestoned':
+        subject = `[${repo(e)}] ${sender(e)} removed the milestone '${e.milestone.title}' from issue #${number(e)}`;
+        message = `[<a href="${repo_url(e)}">${repo(e)}</a>] <a href="${sender_url(e)}">${sender(e)}</a> removed the milestone '<a href="${e.milestone.html_url}">${e.milestone.title}</a>' from issue <a href="${issue_url(e)}">#${number(e)}</a>: <i>${title(e)}</i>`;
+        break;
     }
 
     return {
-        subject: '',
-        message: '',
+        subject: subject,
+        message: message,
     };
 }
 
