f9b8bfebb54d1289b145ca59f1fcceb36b39f141
Ola Bini <ola@olabini.se>
2021-02-11 16:28:35 +0000

    Right, so the this keyboard doesn't work within arrow-functions

--- htmlFormatter.js
+++ htmlFormatter.js
@@ -11,15 +11,15 @@ const formatter = {
     },
     format_link: (link, text) => {
         if (text === '') {
-            return this.format_link(link, link);
+            return formatter.format_link(link, link);
         }
         return `<a href="${link}">${text}</a>`;
     },
-    format_link_simple: (link, text) => this.format_link(link, text),
-    format_front: (e, a) => `[${this.format_link(a.repo_url(e), a.repo(e))}] ${this.format_link(a.sender_url(e), a.sender(e))}`,
+    format_link_simple: (link, text) => formatter.format_link(link, text),
+    format_front: (e, a) => `[${formatter.format_link(a.repo_url(e), a.repo(e))}] ${formatter.format_link(a.sender_url(e), a.sender(e))}`,
     format_issue: (e, a) => {
         const txt = `#${a.number(e)}`;
-        return `${this.format_link(a.issue_url(e), txt)}: ${this.format_emphasis(a.title(e))}`;
+        return `${formatter.format_link(a.issue_url(e), txt)}: ${formatter.format_emphasis(a.title(e))}`;
     },
     format_emphasis: txt => `<i>${txt}</i>`,
 };
